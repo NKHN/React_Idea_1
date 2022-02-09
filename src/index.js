11da6d93ba472cf4fc05d1a1c1b91059126646e6
Nick Hanson <nj_hanson@yahoo.co.uk>
2022-02-09 22:07:37 +0000

    Committing from Sourcetree

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+  <h1>Adding header for commit</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
